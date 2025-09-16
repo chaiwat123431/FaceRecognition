5395bed4af145365b4bd888daa9127a65531def5
ChaiwatAikaew <chaiwatnba@gmail.com>
2025-09-16 16:50:36 -0400

    Update AttendanceProject.py and Attendance.csv

--- venv library root/AttendanceProject.py
+++ venv library root/AttendanceProject.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime
 
 path = 'ImagesAttendances'
 images = []
@@ -21,6 +22,21 @@ def findEncodings(images):
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(BASE_DIR, "Attendance.csv")
+
+def markAttendance(name):
+    with open(file_path,'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
 
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
@@ -43,13 +59,20 @@ while True:
     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        print(faceDis)  
+        #print(faceDis)  
 
         matchIndex = np.argmin(faceDis)
 
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
-            print("Detected:", name)
+            #print("Detected:", name)
+            y1,x2,y2,x1 = faceLoc
+            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            markAttendance(name)
+            
         else:
             print("Unknown")
 
@@ -57,19 +80,6 @@ while True:
     cv2.waitKey(1)
 
 
-
-
-
-#faceLoc = face_recognition.face_locations(imgElon)[0]
-#encodeElon = face_recognition.face_encodings(imgElon)[0]
-#cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0 ]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
-
-#faceLocTest = face_recognition.face_locations(imgTest)[0]
-#encodeTest = face_recognition.face_encodings(imgTest)[0]
-#cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0 ]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
-
-#results = face_recognition.compare_faces([encodeElon],encodeTest)
-#faceDis = face_recognition.face_distance([encodeElon],encodeTest)
 
 
 
